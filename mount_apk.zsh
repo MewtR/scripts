8d115a2a75368a9e1ed81f02ef9c512e082ef2b6
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2022-10-16 11:26:55 -0400

    Finished up script to mount install patched apks, still needs some testing probably

--- mount_apk.zsh
+++ mount_apk.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-# Script to mount install an apk
+# Script to mount install an apk, heavily inspired by some revanced scripts.
 # Pass in the patched apk and the target package.
 if [[ $# -lt 2 ]]; then
     printf "Pass in the patched apk and the target package.\n" >&2
@@ -19,6 +19,20 @@ if [[ $MAGIC_BYTES != $APK_MAGIC_BYTES ]]; then
     exit 1
 fi
 
+adb root
+
+# Check for root
+if ! adb shell su -c exit 2> /dev/null; then
+    printf "You need root for this to work\n"
+    exit 1
+fi
+
+# Check for magisk
+if ! adb shell magisk -c &> /dev/null; then
+    printf "Please have magisk installed\n"
+    exit 1
+fi
+
 RESULTS=$(adb shell pm list packages | grep -i $TARGET_PACKAGE | cut -d: -f2 | wc -l)
 
 if [[ $RESULTS -gt 1 ]]; then
@@ -30,3 +44,32 @@ if [[ $RESULTS -eq 0 ]]; then
     printf "No package found\n" >&2
     exit 1
 fi
+
+TARGET_FULL_NAME=$(adb shell pm list packages | grep -i $TARGET_PACKAGE | sed 's/package://g')
+TARGET_FULL_NAME_APK=$TARGET_FULL_NAME.apk
+printf "Target's full name is: %s\n" $TARGET_FULL_NAME
+TARGET_FULL_PATH=$(adb shell pm path $TARGET_FULL_NAME | grep base | sed 's/package://g')
+printf "Target's full path is: %s\n" $TARGET_FULL_PATH
+
+
+MAGISKTMP=$(adb shell magisk --path)
+MIRROR=$MAGISKTMP/.magisk/mirror
+DEST_DIR=/data/adb/patched
+DEST_FULL_PATH=$DEST_DIR/$TARGET_FULL_NAME_APK
+
+while [ $(adb shell getprop sys.boot_completed | tr -d '\r')  != "1" ]
+do
+    printf "Waiting for device to boot fully...\n"
+    sleep 1;
+done
+
+#Start off by removing any existing links
+adb shell umount -l $TARGET_FULL_PATH
+
+adb shell mkdir -p $DEST_DIR
+adb push $PATCHED_APK $DEST_FULL_PATH
+adb shell chmod 644 $DEST_FULL_PATH
+adb shell chown system:system $DEST_FULL_PATH
+adb shell chcon u:object_r:apk_data_file:s0 $DEST_FULL_PATH
+adb shell mount -o bind $MIRROR$DEST_FULL_PATH $TARGET_FULL_PATH
+
