03a5bc7121a0cb45ae12616824c73ba21f9cfc2a
Mewt R <25155631+MewtR@users.noreply.github.com>
2024-05-25 19:45:52 -0400

    Script to clone some revanced repos, not sure where I'm going with this yet.

---
+++ revanced/clone.zsh
@@ -0,0 +1,33 @@
+#!/usr/bin/zsh
+
+ORG=revanced
+
+CLI="revanced-cli"
+INTEGRATIONS="revanced-integrations"
+PATCHES="revanced-patches"
+PATCHER="revanced-patcher"
+
+REPOS=($CLI $INTEGRATIONS $PATCHES $PATCHER)
+
+INDEX=0
+REPOS_LENGTH=${#REPOS[@]}
+
+for REPO in $REPOS
+do
+    if [[ $INDEX -eq ($REPOS_LENGTH - 1) ]]; then
+        FILTER+=".name == \"${REPO}\" "
+    else
+        FILTER+=".name == \"${REPO}\" or "
+    fi
+    INDEX=$(( INDEX + 1 ))
+done
+
+URLS=$(curl -s https://api.github.com/orgs/${ORG}/repos | jq -r '.[] | {name, ssh_url} | select('${FILTER}') | .ssh_url ' ) #| tr '\n' ' ')
+
+IFS=$'\n' read -r -d '' -A lines <<< $URLS
+for URL in $lines
+do
+    printf "Cloning %s\n\n" $URL
+    git clone --depth=1 $URL
+done
+
