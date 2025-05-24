2e38b161902c132b59dd1b5b031f04294df00e86
Mewt R <25155631+MewtR@users.noreply.github.com>
2025-05-24 11:39:22 -0400

    Logic to parse commandline arguments in order to specify frida version to be downloaded

--- install_frida.sh
+++ install_frida.sh
@@ -1,35 +1,56 @@
 #!/usr/bin/sh
 
+FRIDA=frida
+REPO=$FRIDA/$FRIDA
 ARCH=arm64
-if [ $# -eq 1 ]; then
-    ARCH=$1
-fi
+RELEASES_ENDPOINT=https://api.github.com/repos/${REPO}/releases
+API_URL=${RELEASES_ENDPOINT}/latest
+
+# Parse command line arguments
+# Stolen from here: https://joshtronic.com/2023/03/12/parsing-arguments-in-shell-script/
+while [[ $# -gt 0 ]] do
+    case $1 in
+        --arch)
+            ARCH=$2
+            shift
+            shift
+            ;;
+
+        --tag)
+            API_URL=${RELEASES_ENDPOINT}/tags/$2
+            shift
+            shift
+            ;;
+
+        *)
+            printf "Invalid option\n"
+            exit 1
+    esac
+done
 
 # local install directory
 INSTALL_DIRECTORY=~/tmp/frida_install
 
-FRIDA=frida
 FRIDA_SERVER=$FRIDA-server
 mkdir -p $INSTALL_DIRECTORY
 cd $INSTALL_DIRECTORY
 
 # Download
-REPO=$FRIDA/$FRIDA
-TAG=$(curl -s https://api.github.com/repos/${REPO}/releases/latest | jq -r '.tag_name')
+TAG=$(curl -s $API_URL | jq -r '.tag_name')
 printf "tag name: %s\n" $TAG
 
-NAME=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r --arg arch "$ARCH" '.assets[] | select(.name | startswith("frida-server")) | select(.name | endswith("android-\($arch).xz")) | .name')
+NAME=$(curl -s $API_URL | jq -r --arg arch "$ARCH" '.assets[] | select(.name | startswith("frida-server")) | select(.name | endswith("android-\($arch).xz")) | .name')
 printf "Downloading %s\n" $NAME
 
 
-FRIDA_DOWNLOAD_URL=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r --arg arch "$ARCH" '.assets[] | select(.name | startswith("frida-server")) | select(.name | endswith("android-\($arch).xz")) | .browser_download_url')
+FRIDA_DOWNLOAD_URL=$(curl -s $API_URL | jq -r --arg arch "$ARCH" '.assets[] | select(.name | startswith("frida-server")) | select(.name | endswith("android-\($arch).xz")) | .browser_download_url')
 printf "Download url: %s\n" $FRIDA_DOWNLOAD_URL
 
 wget -O $NAME $FRIDA_DOWNLOAD_URL
 
-unxz $NAME
-NAME_WITHOUT_EXTENSION=$(printf "${NAME%.*}")
-mv $NAME_WITHOUT_EXTENSION $FRIDA_SERVER
+FRIDA_SERVER_XZ=$FRIDA_SERVER.xz
+mv $NAME $FRIDA_SERVER_XZ
+unxz --force $FRIDA_SERVER_XZ
 
 # install directory on my phone
 FRIDA_INSTALL_PATH=/data/local/tmp/$FRIDA_SERVER
